e4d0a2b05507a4a0943bf0db269cbbded5a2c05e
Massimo <massimo.zerbini@gmail.com>
2016-01-20 23:04:27 +0100

    added constants

---
+++ src/ovo/client/model/Constants.java
@@ -0,0 +1,7 @@
+package ovo.client.model;
+
+public class Constants {
+	public static final int MaxNodeNumber = 128;
+    public static final String Active = "ACTIVE";
+    public static final String Inactive = "INACTIVE";
+}
